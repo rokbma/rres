96cd50606a1c7323cc82e037e3d7a385bc5f6526
rokbma <uffdsq@protonmail.com>
2021-11-29 18:06:03 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,170 @@
+use std::fs;
+use std::path;
+use std::process;
+
+use drm::control::{Device as ControlDevice, Mode};
+use drm::Device;
+
+const USAGE: &'static str = "\
+Usage: rres [options]
+
+  -c, --card <card>\tSpecify a GPU (file existing in /dev/dri/, eg. card0)
+  -m, --multi\t\tRead all monitors. If this option is ommited, rres will
+             \t\treturn the resolution of the first detected monitor
+  -v, --verbose\t\tVerbosity level. Can be specified multiple times, e.g. -vv
+  -h, --help\t\tShow this help message
+
+";
+
+pub struct Card(std::fs::File);
+
+impl std::os::unix::io::AsRawFd for Card {
+    fn as_raw_fd(&self) -> std::os::unix::prelude::RawFd {
+        self.0.as_raw_fd()
+    }
+}
+
+impl Card {
+    pub fn open<P: AsRef<path::Path>>(path: P) -> Self {
+        let mut options = std::fs::OpenOptions::new();
+        options.read(true);
+        options.write(true);
+        Card(options.open(path).unwrap())
+    }
+}
+
+impl Device for Card {}
+impl ControlDevice for Card {}
+
+fn main() -> eyre::Result<()> {
+    let mut verbosity = log::LevelFilter::Warn;
+
+    let mut multi = false;
+    let mut card: Option<String> = None;
+
+    {
+        use lexopt::prelude::*;
+        let mut parser = lexopt::Parser::from_env();
+
+        while let Some(arg) = parser.next()? {
+            match arg {
+                Short('m') | Long("multi") => {
+                    multi = true;
+                }
+                Short('c') | Long("card") => {
+                    card = Some(parser.value()?.into_string().unwrap());
+                }
+                Short('h') | Long("help") => {
+                    println!("{}", USAGE);
+                    process::exit(0);
+                }
+                Short('v') | Long("verbose") => {
+                    verbosity = increment_loglevel(verbosity);
+                }
+                _ => panic!("{}", arg.unexpected()),
+            }
+        }
+    }
+
+    pretty_env_logger::formatted_builder()
+        .filter_level(verbosity)
+        .init();
+
+    let mut displays: Vec<Mode> = vec![];
+
+    if let Some(c) = card {
+        let mut file = path::PathBuf::from("/dev/dri/");
+        file.push(&c);
+        if !file.exists() || !c.starts_with("card") {
+            return Err(eyre::eyre!("invalid card ({})", c));
+        }
+
+        let gpu = Card::open(file);
+        let info = gpu.get_driver()?;
+        log::info!("Found GPU: {}", info.name().to_string_lossy());
+        displays.extend_from_slice(&get_card_modes(gpu)?);
+    } else {
+        for entry in fs::read_dir("/dev/dri/")? {
+            let file = entry?;
+
+            if let Some(name) = file.file_name().to_str() {
+                if name.starts_with("card") {
+                    let gpu = Card::open(file.path());
+                    let info = gpu.get_driver()?;
+                    log::info!("Found GPU: {}", info.name().to_string_lossy());
+                    displays.extend_from_slice(&get_card_modes(gpu)?);
+                }
+            }
+        }
+    }
+
+    if displays.len() < 1 {
+        log::error!("Found no display connected!");
+        process::exit(1);
+    }
+
+    if multi {
+        for (i, mode) in displays.iter().enumerate() {
+            let res = mode.size();
+            println!("Display #{}: {}x{}", i, res.0, res.1);
+        }
+    } else {
+        let res = displays[0].size();
+        println!("{}x{}", res.0, res.1);
+    }
+
+    Ok(())
+}
+
+/// Get all the connected display's modes from a libdrm GPU.
+///
+/// Note: nVidia GPUs don't share the current encoder+crtc, so this function will report the
+/// native display's resolution instead of the current resolution.
+pub fn get_card_modes<G: ControlDevice>(gpu: G) -> eyre::Result<Vec<Mode>> {
+    let mut modes: Vec<Mode> = vec![];
+
+    let resources = gpu.resource_handles()?;
+    let connectors = resources.connectors();
+    for handle in connectors {
+        let connector = gpu.get_connector(*handle)?;
+        if connector.state() == drm::control::connector::State::Connected {
+            if let Some(encoder_handle) = connector.current_encoder() {
+                let encoder = gpu.get_encoder(encoder_handle)?;
+                if let Some(crtc_handle) = encoder.crtc() {
+                    let crtc = gpu.get_crtc(crtc_handle)?;
+                    if let Some(current_mode) = crtc.mode() {
+                        log::info!(
+                            "Found display: {:?}, {}x{}",
+                            connector.interface(),
+                            current_mode.size().0,
+                            current_mode.size().1
+                        );
+                        modes.push(current_mode);
+                    }
+                } else {
+                    log::warn!("Could not detect current mode for display {:?},", connector.interface());
+                    log::warn!("reading native resolution");
+                    modes.push(connector.modes()[0]);
+                }
+            } else {
+                log::warn!("Could not detect current mode for display {:?},", connector.interface());
+                log::warn!("reading native resolution");
+                modes.push(connector.modes()[0]);
+            }
+        }
+    }
+    Ok(modes)
+}
+
+/// Increase `log::LevelFilter` by one level
+fn increment_loglevel(level: log::LevelFilter) -> log::LevelFilter {
+    use log::LevelFilter::*;
+    match level {
+        Off => Error,
+        Error => Warn,
+        Warn => Info,
+        Info => Debug,
+        Debug => Trace,
+        Trace => Trace,
+    }
+}
